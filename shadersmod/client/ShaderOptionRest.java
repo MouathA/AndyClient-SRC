8fb2a8d0ea54cf46f9128c9215cd97cd5e2a2900
MouathA <103407967+MouathA@users.noreply.github.com>
2022-06-03 22:02:25 +0400

    Delete shadersmod directory

--- shadersmod/client/ShaderOptionRest.java
+++
@@ -1,8 +0,0 @@
-package shadersmod.client;
-
-public class ShaderOptionRest extends ShaderOption
-{
-    public ShaderOptionRest(final String s) {
-        super(s, s, null, new String[1], null, null);
-    }
-}
